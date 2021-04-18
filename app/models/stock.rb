30c492400a955e2394eece0ea9bf0e3f37ee6c9d
”Ghios” <lubomirshava@gmail.com>
2021-04-18 19:25:02 +0300

    Added JS responces

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,10 @@ class Stock < ApplicationRecord
             secret_token: 'Tsk_1817eda31ed2439ca71952f78539e776',
             endpoint: 'https://sandbox.iexapis.com/v1'
           )
-            client.price(ticker_symbol)
+        begin
+          new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
+        rescue => exception
+          return nil 
+        end
       end
 end
