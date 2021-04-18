65e9aca7cd95db2201d26dfc8ced258aedbb8159
”Ghios” <lubomirshava@gmail.com>
2021-04-18 16:17:47 +0300

    modificated gitignore

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+            publishable_token: 'Tpk_989938e87baf4ffda3615f295f94f1f1',
+            secret_token: 'Tsk_1817eda31ed2439ca71952f78539e776',
+            endpoint: 'https://sandbox.iexapis.com/v1'
+          )
+            client.price(ticker_symbol)
+      end
 end
